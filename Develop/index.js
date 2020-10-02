c00d58f916fe8e48a6b7ff0a90a868204ca7af23
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-10-02 18:24:43 -0400

    I added code to the index with questions, made a new file called readme-template.js, added a package to the file

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,78 @@
-// array of questions for user
-const questions = [
+const fs = require('fs');
 
-];
+const generatePage = require('./src/readme-teplate.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [username, email, title, description, license, install, test, message, contribution] = profileDataArgs;
+
+// array of questions for user
+const questions = () => [
+        /* Pass your questions in here */
+        return inquirer.prompt([
+
+            {type: "input",
+                name: "username",
+                message: "What is your Github username?",
+            },
+
+            {type: "input",
+                name: "email",
+                message: "What is your email address?",
+            },
+
+            {type: "input",
+                name: "title",
+                message: "What is your project's name?",
+            },
+
+            {type: "input",
+                name: "description",
+                message: "Please write a short description of your project",
+            },
+
+            {type: "checkbox",
+                name: "license",
+                message: "What kind of licenses shourld your project have?",
+                choices: 
+                    [
+                        "Artistic license 2.0",
+                        "MIT",
+                        "ISC",
+                        "Microsoft Public license",
+                        "Mozilla Public license",
+                        "Academic Free license",
+                    ],
+            },
+
+            {type: "input",
+                name: "install",
+                message: "What command should be run to install dependencies?",
+            },
+
+            {type: "input",
+                name: "test",
+                message: "what command should be run to run test?",
+            },
+
+            {type: "input",
+                name: "message",
+                message: "what does the user need to know about using the repo?",
+            },
+
+            {type: "input",
+                name: "contribution",
+                message: "What does the iser need to know about contributing to the repo",
+            },
+
+]);
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+fs.writeFile('index.md', generatePage(username, email, title, description, license, install, test, message, contribution), => err {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 // function to initialize program
 function init() {
---
+++ src/readme-template.js
@@ -0,0 +1,62 @@
+const generatePage = 
+    (username,
+        email, 
+        title, 
+        description, 
+        license, 
+        install, 
+        test, 
+        message, 
+        contribution) => {
+    return `
+    # ${title}
+    ![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+    ## Description
+
+    ${description}
+
+    ## Table of Contents
+
+    * [Installation](#installation)
+
+    * [Usage](#usage)
+
+    * [License](#license)
+
+    * [Contributing](#contributing)
+
+    * [Test](#test)
+
+    * [Questions](#questions)
+
+    ## Installation
+
+    To Install necessary dependencies, run the following command:
+    
+    '''
+    ${install}
+    '''
+
+    ## Usage
+
+    ${message}
+
+    ## License
+
+    ## Contributing
+
+    ${contribution}
+
+    ## Test
+
+    To run the tests, run the following command:
+
+    '''
+    ${test}
+    '''
+  `;
+
+};
+
+module.exports = generatePage;
